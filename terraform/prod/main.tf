48e61e77216c19e53a58bb81f848f24238265550
Ишмухаметов Ильшат <IIshmukhametov@sberbank.ru>
2022-10-04 22:45:49 +0300

    terraform-2 homework

---
+++ terraform/prod/main.tf
@@ -0,0 +1,34 @@
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
+
+resource "yandex_vpc_network" "app-network" {
+  name = "reddit-app-network-prod"
+}
+
+resource "yandex_vpc_subnet" "app-subnet" {
+  name           = "reddit-app-subnet-prod"
+  zone           = "ru-central1-a"
+  network_id     = "${yandex_vpc_network.app-network.id}"
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+module "db" {
+  source          = "../modules/db"
+  public_key_path = var.public_key_path
+  db_disk_image   = var.db_disk_image
+  subnet_id       = yandex_vpc_subnet.app-subnet.id
+  resource_name   = "db-prod"
+}
+
+module "app" {
+  source          = "../modules/app"
+  public_key_path = var.public_key_path
+  app_disk_image  = var.app_disk_image
+  subnet_id       = yandex_vpc_subnet.app-subnet.id
+  resource_name   = "app-prod"
+  database_url    = module.db.internal_ip_address_db
+}
